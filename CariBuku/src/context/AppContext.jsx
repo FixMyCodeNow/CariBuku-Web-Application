e392dcbc656992c136a59d0c7223376023ac62ff
FixMyCodeNow <153930601+FixMyCodeNow@users.noreply.github.com>
2025-08-22 19:16:32 +0700

    Add files via upload

---
+++ CariBuku/src/context/AppContext.jsx
@@ -0,0 +1,57 @@
+import React, { useState, useEffect, useCallback } from "react";
+
+const URL = "https://openlibrary.org/search.json?title=";
+const AppContext = React.createContext();
+
+const AppProvider = ({ children }) => {
+  const [searchTerm, setSearchTerm] = useState("Find Your Book");
+  const [books, setBooks] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [resultTitle, setResultTitle] = useState("");
+
+  const fetchBooks = useCallback(async () => {
+    setLoading(true);
+    try {
+      const response = await fetch(`${URL}${searchTerm}`);
+      const data = await response.json();
+      const { docs } = data;
+
+      if (docs) {
+        const newBooks = docs.slice(0, 20).map((bookSingle) => {
+          const { key, author_name, cover_i, edition_count, first_publish_year, title } = bookSingle;
+
+          return {
+            id: key,
+            author: author_name,
+            cover_id: cover_i,
+            edition_count,
+            first_publish_year,
+            title,
+          };
+        });
+
+        setBooks(newBooks);
+        setResultTitle(newBooks.length > 1 ? "Your Search Result" : "No Search Result Found!");
+      } else {
+        setBooks([]);
+        setResultTitle("No Search Result Found!");
+      }
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+    }
+  }, [searchTerm]);
+
+  useEffect(() => {
+    fetchBooks();
+  }, [searchTerm, fetchBooks]);
+
+  return (
+    <AppContext.Provider value={{ loading, books, setSearchTerm, resultTitle, setResultTitle }}>
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+export { AppContext, AppProvider };
